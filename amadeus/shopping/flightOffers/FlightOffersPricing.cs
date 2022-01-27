d85b3c9b45f2511b3fc2b4d96f55cf893ab0ffb4
Tiago Fernandes <tiago.s.fernandes@cgi.com>
2022-01-27 15:43:03 +0000

    Updated pricing to only receive body

--- amadeus/shopping/flightOffers/FlightOffersPricing.cs
+++ amadeus/shopping/flightOffers/FlightOffersPricing.cs
@@ -36,16 +36,42 @@ namespace amadeus.shopping.flightOffers
         /// </para>
         /// 
         /// <code>
-        /// amadeus.shopping.flightOffers.pricing.postFlightOffersPricing(body);
+        /// amadeus.shopping.flightOffers.pricing.postFlightOffersPricing(_params, body);
         /// </code>
         /// </summary>
         /// <returns>an API response object</returns>
         /// <param name="_params">body the parameters to send to the API as a JSonObject</param>
+        /// <param name="body">body the parameters to send to the API as a JSonObject</param>
         public FlightOfferPricingOutput postFlightOffersPricing(Params _params, string body)
         {
             try
             {
                 Response response = client.post("/v1/shopping/flight-offers/pricing", _params, body);
+
+                return (FlightOfferPricingOutput)Resource.fromObject(response, typeof(FlightOfferPricingOutput));
+            }
+            catch (ResponseException)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// <para>
+        /// /shopping/flight-offers/pricing.
+        /// </para>
+        /// 
+        /// <code>
+        /// amadeus.shopping.flightOffers.pricing.postFlightOffersPricing(body);
+        /// </code>
+        /// </summary>
+        /// <returns>an API response object</returns>
+        /// <param name="body">body the parameters to send to the API as a JSonObject</param>
+        public FlightOfferPricingOutput postFlightOffersPricing(string body)
+        {
+            try
+            {
+                Response response = client.post("/v1/shopping/flight-offers/pricing", body);
 
                 return (FlightOfferPricingOutput)Resource.fromObject(response, typeof(FlightOfferPricingOutput));
             }
